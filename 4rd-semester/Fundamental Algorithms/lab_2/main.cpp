923bb91cef08e89e1d125b6336cb2bc19946d238
myPC <artem@ErrorPC.localdomain>
2022-03-20 14:27:52 +0300

    FA::add map to FA lab_2

---
+++ 4rd-semester/Fundamental Algorithms/lab_2/main.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <string>
+
+#include "include/Memory/memory.h"
+// TODO: добавить парсер
+
+using u8 = uint8_t;
+using u16 = uint16_t;
+using u32 = uint32_t;
+using u64 = uint64_t;
+
+
+int main(int argc, char const *argv[])
+{
+    
+    return 0;
+}
